d94006e23e77fc61045fe0a9f8822f328657feb1
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-01 15:15:24 +0600

    category add page created

--- admin/catadd.php
+++ admin/catadd.php
@@ -1,14 +1,30 @@
-﻿<?php include 'inc/header.php';?>
+﻿<?php
+    include '../classes/Category.php';
+?>
+<?php
+    $cat = new Category();
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+        $catName = $_POST['catName'];
+
+        $insertCat = $cat->catInsert($catName);
+    }
+?>
+<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
         <div class="grid_10">
             <div class="box round first grid">
                 <h2>Add New Category</h2>
                <div class="block copyblock"> 
-                 <form>
+               <?php
+                if (isset($insertCat)) {
+                    echo $insertCat;
+                }
+               ?>
+                    <form method="post" action="">
                     <table class="form">					
                         <tr>
                             <td>
-                                <input type="text" placeholder="Enter Category Name..." class="medium" />
+                                <input type="text" name="catName" placeholder="Enter Category Name..." class="medium" />
                             </td>
                         </tr>
 						<tr> 
