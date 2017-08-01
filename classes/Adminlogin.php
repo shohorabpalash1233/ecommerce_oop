125a7da08f41e1262bddd3e1b43e9ac7b7e7d484
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-01 16:13:48 +0600

    Adminlogin file modified-realpath added

--- classes/Adminlogin.php
+++ classes/Adminlogin.php
@@ -1,8 +1,9 @@
 <?php
-	include '../lib/Session.php';
+	$filepath = realpath(dirname(__FILE__));
+	include_once ($filepath.'/../lib/Session.php');
 	Session::checkLogin();
-	include '../lib/Database.php';
-	include '../helper/Format.php';
+	include_once ($filepath.'/../lib/Database.php');
+	include_once ($filepath.'/../helper/Format.php');
 ?>
 <?php
 
