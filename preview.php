3ee1fce6fb96024c525b596920ac196715d734c7
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-13 16:59:02 +0600

    add to wishlist and compare button created

--- preview.php
+++ preview.php
@@ -2,19 +2,34 @@
 	include 'inc/header.php';
 ?>
 <?php
-
-    if (!isset($_GET['proid']) || $_GET['proid'] == NULL) {
-        echo "<script>window.location = '404.php'; </script>";
-    } else {
+    if (isset($_GET['proid']) || $_GET['proid'] == NULL) {
         $id = $_GET['proid'];
         $id = preg_replace('/[^-a-zA-Z0-9_]/', '', $_GET['proid']);
     }
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['submit'])) {
     	$quantity = $_POST['quantity'];
     	$addCart = $cart->addToCart($quantity, $id);
     }
 ?>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['compare'])) {
+    	$productId = $_POST['productId'];
+        $insertCom = $product->insertCompareData($productId, $cusid);
+    }
+?>
+<?php
+    if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['wlist'])) {
+        $saveWlist = $product->saveWishlistData($id, $cusid);
+    }
+?>
 
+<style type="text/css" media="screen">
+	.mybutton{
+		width: 100px;
+		float: left;
+		margin-right: 50px;
+	}
+</style>
 
  <div class="main">
     <div class="content">
@@ -49,6 +64,35 @@
 						}
 					?>
 				</span>
+				<?php
+						if (isset($insertCom)) {
+							echo $insertCom;
+						}
+						if (isset($saveWlist)) {
+							echo $saveWlist;
+						}
+					?>
+					<?php
+						$login = Session::get("cuslogin");
+	  					if ($login == true) {
+	  		
+					?>
+				<div class="add-cart">
+				<div class="mybutton">
+				<form action="" method="post">
+					<input type="hidden" class="buyfield" name="productId" value="<?php echo $result['productId'];?>"/>
+					<input type="submit" class="buysubmit" name="compare" value="Add To Compare"/>
+				</form>		
+				</div>
+				<div class="mybutton">
+					<form action="" method="post">
+					<input type="submit" class="buysubmit" name="wlist" value="Add To Wishlist"/>
+				</form>	
+				</div>
+				
+
+				</div>
+				<?php } ?>
 			</div>
 			<div class="product-desc">
 			<h2>Product Details</h2>
