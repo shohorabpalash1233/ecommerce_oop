967f2d3073b4256c1a29861a8bf1eefe2b9a5d93
shohorabpalash1233 <shohorabpalash1@gmail.com>
2017-08-13 16:55:38 +0600

    payment confirmation system added for admin

--- admin/inbox.php
+++ admin/inbox.php
@@ -1,58 +1,90 @@
 ﻿<?php include 'inc/header.php';?>
 <?php include 'inc/sidebar.php';?>
+<?php 
+include '../classes/Cart.php';
+$fm = new Format();
+$cart = new Cart();
+?>
+<?php
+	if (isset($_GET['shipId'])) {
+		$id = $_GET['shipId'];
+		$date = $_GET['date'];
+		$price = $_GET['price'];
+
+		$shipped = $cart->productShipped($id, $date, $price);
+	}
+
+	if (isset($_GET['delId'])) {
+		$id = $_GET['delId'];
+		$date = $_GET['date'];
+		$price = $_GET['price'];
+
+		$delOrder = $cart->delProductShipped($id, $date, $price);
+	}
+?>
+
         <div class="grid_10">
             <div class="box round first grid">
                 <h2>Inbox</h2>
+                <?php
+                	if (isset($shipped)) {
+                		echo $shipped;
+                	}
+                	if (isset($delOrder)) {
+                		echo $delOrder;
+                	}
+                ?>
                 <div class="block">        
                     <table class="data display datatable" id="example">
 					<thead>
 						<tr>
-							<th>Serial No.</th>
-							<th>Message</th>
+							<th>ID</th>
+							<th>Order Time</th>
+							<th>Product</th>
+							<th>Quantity</th>
+							<th>Price</th>
+							<th>Customer ID</th>
+							<th>Address</th>
 							<th>Action</th>
 						</tr>
 					</thead>
 					<tbody>
+					<?php
+						
+						$getOrder = $cart->getAllOrderProduct();
+						if ($getOrder) {
+							while ($result = $getOrder->fetch_assoc()) {
+								
+							
+					?>
 						<tr class="odd gradeX">
-							<td>01</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
+							<td><?php echo $result['id']; ?></td>
+							<td><?php echo $fm->formatDate($result['date']); ?></td>
+							<td><?php echo $result['productName']; ?></td>
+							<td><?php echo $result['quantity']; ?></td>
+							<td>$<?php echo $result['price']; ?></td>
+							<td><?php echo $result['cusId']; ?></td>
+							<td><a href="customer.php?custId=<?php echo $result['cusId']; ?>" title="">View Details</a></td>
+							<?php
+								if ($result['status'] == '0') {?>
+								<td><a href="?shipId=<?php echo $result['cusId']; ?>&price=<?php echo $result['price']; ?>&date=<?php echo $result['date'];?>">Shipped</a></td>
+									<?php
+								} elseif($result['status'] == '1') {
+									?>
+									<td>Pending</td>
+									<?php
+								} else {
+									?>
+									<td><a href="?delId=<?php echo $result['cusId']; ?>&price=<?php echo $result['price']; ?>&date=<?php echo $result['date'];?>">Remove</a></td>
+									<?php
+								}
+							?>
+							
 						</tr>
-						<tr class="even gradeC">
-							<td>02</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>03</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>04</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-							<tr class="odd gradeX">
-							<td>05</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>06</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="odd gradeX">
-							<td>07</td>
-							<td>Internet</td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
-						<tr class="even gradeC">
-							<td>08</td>
-							<td>Explorer </td>
-							<td><a href="">Edit</a> || <a href="">Delete</a></td>
-						</tr>
+						<?php
+							}
+						}
+						?>
 					</tbody>
 				</table>
                </div>
